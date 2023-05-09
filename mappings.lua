08ce8948512d7e7e7c0be8a701d02d4ff3e46d0d
Brian Gwaltney <brian.gwaltney@gmail.com>
2023-05-09 13:38:05 +0300

    init commit

---
+++ mappings.lua
@@ -0,0 +1,155 @@
+---@type MappingsTable
+local M = {}
+
+M.disabled = {
+  n = {
+    -- Disable
+    ["<leader>n"] = { "" },
+  },
+}
+
+M.general = {
+  n = {
+    ["<c-Y>"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflinePrev()
+      end,
+      "goto prev buffer",
+    },
+
+    ["<Esc>"] = { ":noh<CR>:w<CR>", "clear highlights and save" },
+
+    ["<leader>w"] = {
+
+      function()
+        require("nvchad_ui.tabufline").close_buffer()
+      end,
+      "close buffer",
+    },
+
+    ["{"] = { "<C-u>zz", "half page up" },
+
+    ["}"] = { "<C-d>zz", "half page down" },
+    ["U"] = { "<C-r>", "undo" },
+    ["n"] = { "nzzzv", "find next and center" },
+    ["N"] = { "Nzzzv", "find next and center" },
+    ["gc"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+
+    ["L"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflineNext()
+      end,
+      "goto next buffer",
+    },
+
+    ["H"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflinePrev()
+      end,
+      "goto prev buffer",
+    },
+
+    ["<leader>ch"] = { "<cmd> Telescope keymaps <CR>", "Keymap search" },
+
+    -- Harpoon
+    ["<leader>9"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      "add harpoon",
+    },
+    ["<leader>0"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      "harpoon menu",
+    },
+    ["<leader>1"] = {
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      "harpoon file 1",
+    },
+    ["<leader>2"] = {
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      "harpoon file 2",
+    },
+    ["<leader>3"] = {
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      "harpoon file 3",
+    },
+    ["<leader>5"] = {
+      function()
+        require("harpoon.ui").nav_file(5)
+      end,
+      "harpoon file 5",
+    },
+    ["<leader>6"] = {
+      function()
+        require("harpoon.ui").nav_file(6)
+      end,
+      "harpoon file 6",
+    },
+  },
+
+  v = {
+    ["gc"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
+  },
+
+  i = {
+    ["<C-f>"] = {
+      function()
+        local cmp = require "cmp"
+        if cmp.visible() then
+          cmp.close()
+        else
+          cmp.complete()
+        end
+      end,
+    },
+    ["<Up>"] = {
+      function()
+        local cmp = require "cmp"
+        if cmp.visible() then
+          cmp.select_prev_item()
+        end
+      end,
+      "Cycle autocomplete",
+    },
+    ["<Down>"] = {
+      function()
+        local cmp = require "cmp"
+        if cmp.visible() then
+          cmp.select_next_item()
+        end
+      end,
+      "Cycle autocomplete",
+    },
+
+    ["<C-Y>"] = {
+      function()
+        local cmp = require "cmp"
+        if cmp.visible() then
+          cmp.select_prev_item()
+        end
+      end,
+      "testing",
+    },
+  },
+}
+
+-- more keybinds!
+
+return M
---
+++ plugins.lua
@@ -0,0 +1,86 @@
+local overrides = require "custom.configs.overrides"
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "copilot" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
+      },
+    },
+  },
+  {
+    "ThePrimeagen/harpoon"
+  }
+}
+
+return plugins
